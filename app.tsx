0811567f37b9ada6b7f1db88697b93f59024ebce
Rahul Guha <69rahul16@gmail.com>
2024-04-30 20:21:50 +0530

    pls 0.5 ETH

---
+++ app.tsx
@@ -0,0 +1,90 @@
+import React, { ReactElement, useCallback, useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import { prove, verify } from 'tlsn-js';
+import { Proof } from 'tlsn-js/build/types';
+import { Watch } from 'react-loader-spinner'
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(<App />);
+
+function App(): ReactElement {
+  const [processing, setProcessing] = useState(false);
+  const [result, setResult] = useState<{
+    time: number;
+    sent: string;
+    recv: string;
+    notaryUrl: string;
+  } | null>(null);
+  const [proof, setProof] = useState<Proof | null>(null);
+///https://taipei-backend.vercel.app/api/sign-token/credit/
+///      method: 'POST',
+////body:`{"name":"Rahul","id":69420}`,
+  const onClick = useCallback(async () => {
+    setProcessing(true);
+    const p = await prove('https://taipei-backend.vercel.app/api/sign-token/credit', {
+      method: 'POST',
+      maxTranscriptSize: 16384,
+      notaryUrl: 'https://notary.pse.dev',
+      websocketProxyUrl: 'ws://localhost:55688',
+      body: '{"name": "Rahul", "id": 69420}'
+    });
+    setProof(p);
+  }, [setProof, setProcessing]);
+
+  useEffect(() => {
+    (async () => {
+      if (proof) {
+        const r = await verify(proof);
+        setResult(r);
+        setProcessing(false);
+      }
+    })();
+  }, [proof, setResult]);
+
+  return (
+    <div>
+      <button onClick={!processing ? onClick : undefined} disabled={processing}>
+        Start demo
+      </button>
+      <div>
+        <b>Proof: </b>
+        {!processing && !proof
+          ? <i>not started</i>
+          : !proof
+            ? <>
+              Proving data from swapi...
+              <Watch
+                visible={true}
+                height="40"
+                width="40"
+                radius="48"
+                color="#000000"
+                ariaLabel="watch-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+              />
+              Open <i>Developer tools</i> to follow progress
+            </>
+            : <>
+              <details>
+                <summary>View Proof</summary>
+                <pre>{JSON.stringify(proof, null, 2)}</pre>
+              </details>
+            </>
+        }
+
+      </div>
+      <div>
+        <b>Verification: </b>
+        {!proof
+          ? <i>not started</i>
+          : !result
+            ? <i>verifying</i>
+            : <pre>{JSON.stringify(result, null, 2)}</pre>
+        }
+      </div>
+    </div>
+  );
+}
